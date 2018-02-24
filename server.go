d7b588dfeb664a8880e1e56cc5398c9becd5b5a4
John Maguire <john.maguire@gmail.com>
2018-02-24 21:18:05 +0000

    Hacky but works with github app auth

--- server.go
+++ server.go
@@ -13,7 +13,9 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/pmezard/go-difflib/difflib"
 	"sourcegraph.com/sourcegraph/go-diff/diff"
@@ -27,12 +29,15 @@ var (
 	style = flag.String("style",
 		"{BasedOnStyle: Google, DerivePointerBinding: false, Standard: Cpp11}",
 		"Style specification passed to clang-format")
-	token = flag.String("github", "", "Github personal access token")
+	token      = flag.String("github", "", "Github personal access token")
+	privateKey = flag.String("private-key", "", "Path to github app private key")
 )
 
 const (
-	pullRequestsURL     = "https://api.github.com/repos/%s/%s/pulls"
-	pullRequestFilesURL = "https://api.github.com/repos/%s/%s/pulls/%d/files"
+	pullRequestsURL      = "https://api.github.com/repos/%s/%s/pulls"
+	pullRequestFilesURL  = "https://api.github.com/repos/%s/%s/pulls/%d/files"
+	listInstallationsURL = "https://api.github.com/app/installations"
+	installationTokenURL = "https://api.github.com/installations/%d/access_tokens"
 )
 
 type PullRequest struct {
@@ -51,6 +56,109 @@ type PullRequestFile struct {
 	BlobURL  string `json:"blob_url"`
 	RawURL   string `json:"raw_url"`
 	Patch    string
+}
+
+type Installation struct {
+	ID      int     `json:"id"`
+	Account Account `json:"account"`
+}
+
+type Account struct {
+	Login string `json:"login"`
+}
+
+type InstallationToken struct {
+	Token     string
+	ExpiresAt time.Time
+}
+
+func createJWT(keyPath string) (string, error) {
+	keyData, err := ioutil.ReadFile(keyPath)
+	if err != nil {
+		return "", fmt.Errorf("Failed to load private key for signing github tokens: %v", err)
+	}
+
+	key, err := jwt.ParseRSAPrivateKeyFromPEM(keyData)
+	if err != nil {
+		return "", fmt.Errorf("Failed to parse RSA key from file: %v", err)
+	}
+
+	t := time.Now()
+	claim := jwt.MapClaims{
+		"iat": t.Unix(),
+		"exp": t.Add(time.Minute * 10).Unix(),
+		"iss": 9459,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claim)
+	ret, err := token.SignedString(key)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign github token request: %v", err)
+	}
+	log.Printf("JWT: %s", ret)
+	return ret, nil
+}
+
+func getInstallationID(owner string) (int, error) {
+	req, _ := http.NewRequest("GET", listInstallationsURL, nil)
+	t, err := createJWT(*privateKey)
+	if err != nil {
+		return -1, fmt.Errorf("failed to create JWT: %v", err)
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", t))
+	req.Header.Set("Accept", "application/vnd.github.machine-man-preview+json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return -1, fmt.Errorf("Failed to list app installations: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		b, _ := ioutil.ReadAll(resp.Body)
+		return -1, fmt.Errorf("Failed to list app installations: %s %s", resp.Status, b)
+	}
+
+	dec := json.NewDecoder(resp.Body)
+	var installs []Installation
+	err = dec.Decode(&installs)
+	log.Printf("Installations: %v", installs)
+	if err != nil {
+		log.Printf("Decoding JSON failed: %+v", err)
+		return -1, fmt.Errorf("Decoding JSON failed: %v", err)
+	}
+
+	for _, install := range installs {
+		if install.Account.Login == owner {
+			return install.ID, nil
+		}
+	}
+	return -1, fmt.Errorf("No permissions for owner: %s", owner)
+}
+
+func createTokenForInstallation(owner string) (string, error) {
+	installID, err := getInstallationID(owner)
+	if err != nil {
+		return "", fmt.Errorf("Failed to get ID for installation: %s %v", owner, err)
+	}
+	t, err := createJWT(*privateKey)
+	if err != nil {
+		return "", fmt.Errorf("Failed to create JWT: %v", err)
+	}
+	req, _ := http.NewRequest("POST", fmt.Sprintf(installationTokenURL, installID), nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", t))
+	req.Header.Set("Accept", "application/vnd.github.machine-man-preview+json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("Failed to get installation token: %v", err)
+	}
+	defer resp.Body.Close()
+
+	dec := json.NewDecoder(resp.Body)
+	var token InstallationToken
+	err = dec.Decode(&token)
+	if err != nil {
+		return "", fmt.Errorf("Decoding JSON failed: %v", err)
+	}
+	return token.Token, nil
 }
 
 func format(r io.Reader, hunks []*diff.Hunk) ([]byte, error) {
@@ -146,13 +254,26 @@ func githubHandler(w http.ResponseWriter, r *http.Request) {
 
 func CheckPullRequest(owner string, repo string, number int) (string, error) {
 	req, _ := http.NewRequest("GET", fmt.Sprintf(pullRequestFilesURL, owner, repo, number), nil)
-	req.Header.Add("Authorization", fmt.Sprintf("token %s", *token))
+	token, err := createTokenForInstallation(owner)
+	if err != nil {
+		return "", fmt.Errorf("Failed to create token for github request: %v", err)
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("token %s", token))
+	log.Printf("Auth: %s", req.Header.Get("Authorization"))
+	req.Header.Set("Accept", "application/vnd.github.machine-man-preview+json")
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Printf("Request failed: %+v", err)
 		return "", fmt.Errorf("Request failed: %v", err)
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Printf("Github request status: %d %s", resp.StatusCode, resp.Status)
+		body, _ := ioutil.ReadAll(resp.Body)
+		return "", fmt.Errorf("Request failed: %s", body)
+	}
+
 	dec := json.NewDecoder(resp.Body)
 	var listFiles []PullRequestFile
 	err = dec.Decode(&listFiles)
@@ -167,7 +288,8 @@ func CheckPullRequest(owner string, repo string, number int) (string, error) {
 			continue
 		}
 		r, _ := http.NewRequest("GET", file.RawURL, nil)
-		r.Header.Add("Authorization", fmt.Sprintf("token %s", *token))
+		r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+		r.Header.Set("Accept", "application/vnd.github.machine-man-preview+json")
 		resp, err := http.DefaultClient.Do(r)
 		if err != nil {
 			log.Printf("Request failed: %+v", err)
