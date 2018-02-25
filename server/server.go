a3a315a7e4ecf19cce890a8f7bf090e26fc14064
John Maguire <john.maguire@gmail.com>
2018-02-25 00:56:06 +0000

    Re-use github API client

--- server/server.go
+++ server/server.go
@@ -45,34 +45,23 @@ func formatHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func githubClementineHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(mux.Vars(r)["id"])
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	c := github.NewAPIClientFromFile(*privateKey)
-	diff, err := c.CheckPullRequest("clementine-player", "clementine", id)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	html, err := formatAsHTML(diff)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	io.WriteString(w, html)
+type githubHandler struct {
+	githubClient *github.APIClient
 }
 
-func githubHandler(w http.ResponseWriter, r *http.Request) {
+func newGithubHandler() *githubHandler {
+	return &githubHandler{
+		githubClient: github.NewAPIClientFromFile(*privateKey),
+	}
+}
+
+func (h *githubHandler) pullRequestHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(mux.Vars(r)["id"])
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	c := github.NewAPIClientFromFile(*privateKey)
-	diff, err := c.CheckPullRequest(mux.Vars(r)["owner"], mux.Vars(r)["repo"], id)
+	diff, err := h.githubClient.CheckPullRequest(mux.Vars(r)["owner"], mux.Vars(r)["repo"], id)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -86,17 +75,42 @@ func githubHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, html)
 }
 
-func updatePullRequestStatus(owner string, repo string, number int) error {
-	c := github.NewAPIClientFromFile(*privateKey)
-	unifiedDiff, err := c.CheckPullRequest(owner, repo, number)
+func (h *githubHandler) pushHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	defer r.Body.Close()
+	dec := json.NewDecoder(r.Body)
+	var push github.Webhook
+	err := dec.Decode(&push)
+	if err != nil {
+		http.Error(w, "Failed to parse webhook", http.StatusInternalServerError)
+		return
+	}
+	log.Printf("Webhook: %+v", push)
+
+	// Ignore anything that is not a pull request event.
+	if push.PullRequest == nil {
+		return
+	}
+	if push.Action != "opened" && push.Action != "synchronize" {
+		return
+	}
+
+	go h.updatePullRequestStatus(push.Repository.Owner.Login, push.Repository.Name, push.Number)
+}
+
+func (h *githubHandler) updatePullRequestStatus(owner string, repo string, number int) error {
+	unifiedDiff, err := h.githubClient.CheckPullRequest(owner, repo, number)
 	if err != nil {
 		return err
 	}
 
 	if len(unifiedDiff) == 0 {
-		err = c.PostSuccessStatus(owner, repo, number)
+		err = h.githubClient.PostSuccessStatus(owner, repo, number)
 	} else {
-		err = c.PostFailureStatus(owner, repo, number)
+		err = h.githubClient.PostFailureStatus(owner, repo, number)
 	}
 	return err
 }
@@ -129,40 +143,15 @@ func formatAsHTML(diff string) (string, error) {
 	return buf.String(), nil
 }
 
-func githubPushHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		return
-	}
-	defer r.Body.Close()
-	dec := json.NewDecoder(r.Body)
-	var push github.Webhook
-	err := dec.Decode(&push)
-	if err != nil {
-		http.Error(w, "Failed to parse webhook", http.StatusInternalServerError)
-		return
-	}
-	log.Printf("Webhook: %+v", push)
-
-	// Ignore anything that is not a pull request event.
-	if push.PullRequest == nil {
-		return
-	}
-	if push.Action != "opened" && push.Action != "synchronize" {
-		return
-	}
-
-	go updatePullRequestStatus(push.Repository.Owner.Login, push.Repository.Name, push.Number)
-}
-
 func main() {
 	flag.Parse()
 
+	handler := newGithubHandler()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/format", formatHandler)
-	r.HandleFunc("/github/{id}", githubClementineHandler)
-	r.HandleFunc("/github/{owner}/{repo}/{id}", githubHandler)
-	r.HandleFunc("/github-push", githubPushHandler)
+	r.HandleFunc("/github/{owner}/{repo}/{id}", handler.pullRequestHandler)
+	r.HandleFunc("/github-push", handler.pushHandler)
 	log.Print("Starting server...")
 	http.Handle("/", r)
 	http.ListenAndServe(*address+":"+strconv.Itoa(*port), nil)
