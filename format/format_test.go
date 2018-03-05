7c648eda782362e7ff39effc7886270ce60a6c65
John Maguire <john.maguire@gmail.com>
2018-03-05 17:25:30 +0000

    Add test for diff parsing

---
+++ format/format_test.go
@@ -0,0 +1,31 @@
+package format
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"sourcegraph.com/sourcegraph/go-diff/diff"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestAdditions(t *testing.T) {
+	Convey("", t, func() {
+		data, err := ioutil.ReadFile("6011.patch")
+		So(err, ShouldBeNil)
+		diffs, err := diff.ParseMultiFileDiff(data)
+		So(err, ShouldBeNil)
+		So(diffs, ShouldHaveLength, 2)
+
+		hunks := diffs[0].Hunks
+
+		So(hasAdditions(hunks[0]), ShouldBeTrue)
+		So(hasAdditions(hunks[1]), ShouldBeTrue)
+		So(hasAdditions(hunks[2]), ShouldBeTrue)
+
+		additions := findAdditions(hunks[0])
+		So(additions, ShouldHaveLength, 1)
+		So(additions[0].Start, ShouldEqual, 3)
+		So(additions[0].End, ShouldEqual, 3)
+	})
+}
